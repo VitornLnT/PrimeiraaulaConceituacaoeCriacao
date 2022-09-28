b9c4ef24b3e09df5e09663dec12546273684b476
Vitor <vitor.vao.no87@gmail.com>
2022-09-28 14:42:50 -0300

    Aula de metodos

---
+++ src/br/com/dio/sobrecarga/Main.java
@@ -0,0 +1,15 @@
+package br.com.dio.sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Quadrilatero
+        System.out.println("Exercício Quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
